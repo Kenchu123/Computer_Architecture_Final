4e12286792a863ca2529ae772c0131fcdc3dabe5
fanyungwei <brian.155406@gmail.com>
2020-12-25 02:38:19 +0800

    can compile

--- RISCV.v
+++ RISCV.v
@@ -15,8 +15,8 @@ module RISCV(clk,
     // Input/Output Part
     input             clk, rst_n;   // Clock signal, Active low asyncrhonous reset signal
     input      [31:0] mem_rdata_I;  // Instruction read from instruction memory
-    output reg [29:0] mem_addr_I;   // Output address of instruction memory
-    output     [31:0] mem_addr_D;   // Output address of data memory
+    output reg [31:2] mem_addr_I;   // Output address of instruction memory
+    output     [31:2] mem_addr_D;   // Output address of data memory
     output            mem_wen_D;    // Write-enable
     output     [63:0] mem_wdata_D;  // Data store to data memory
     input      [63:0] mem_rdata_D;  // Data read from data memory
@@ -32,7 +32,7 @@ module RISCV(clk,
     parameter B = 5'b00010;
     parameter J = 5'b00001;
 
-    parameter JAL  = {1'b1, 22'b0}      ;       // 1101111
+    parameter JAL  = {1'b1, 22'b0};             // 1101111
     parameter JALR = {1'b0, 1'b1, 21'b0};       // 1100111 000
     parameter BEQ  = {2'b0, 1'b1, 20'b0};       // 1100011 000
     parameter BNE  = {3'b0, 1'b1, 19'b0};       // 1100011 001
@@ -76,6 +76,8 @@ module RISCV(clk,
     wire [63:0] read_data1, read_data2;
     wire [4:0]  read_reg1, read_reg2, write_reg;
     wire [63:0] write_data;
+    wire [63:0] mem_rdata_D_BE; // Big Endian
+    wire [63:0] mem_wdata_D_LE; // Little Endian
 
     wire [63:0] reg_or_imm; // ALU data2
     wire [63:0] ALU_result;
@@ -86,8 +88,6 @@ module RISCV(clk,
 
     // Reg Part
     reg  [29:0] next_mem_addr_I;
-    // reg        next_mem_wen_D;
-    // reg [63:0] next_mem_wdata_D;
     
     // Assign Part
 
@@ -103,23 +103,24 @@ module RISCV(clk,
     assign ALUOp    =   ctrl[3:0];
 
         // assign others
-    assign read_reg1 = mem_addr_I[19:15];
-    assign read_reg2 = mem_addr_I[24:20];
-    assign write_reg = mem_addr_I[11:7];
-    assign write_data = (MemtoReg)? mem_rdata_D : ALU_result;
+    assign read_reg1 = {mem_rdata_I[11:8], mem_rdata_I[23]};   // Little Endian
+    assign read_reg2 = {mem_rdata_I[0], mem_rdata_I[15:12]};   // Little Endian
+    assign write_reg = {mem_rdata_I[19:16], mem_rdata_I[31]};  // Little Endian
+    assign write_data = (Jal || Jalr)? {34'b0, (mem_addr_I + 1)} : ((MemtoReg)? mem_rdata_D_BE : ALU_result);
 
     assign reg_or_imm = (ALUSrc)? imm : read_data2;
-    assign ALU_ctrl = (Branch)? 4'b1000 : ((MemRead || MemWrite)? 4'b0000: ALUOp); // branch is SUB, ld/sd are ADD
+    assign ALU_ctrl = (Branch)? 4'b1000 : ((MemRead || MemWrite)? 4'b0000 : ALUOp); // branch is SUB, ld/sd are ADD
 
     assign shifted_imm = imm << 1;
 
         // assign output
     assign mem_wen_D = MemWrite;
-    assign mem_addr_D = ALU_result;
-    assign mem_wdata_D = read_data2;
+    assign mem_addr_D = ALU_result >> 2;
+    assign mem_wdata_D = mem_wdata_D_LE;
 
     // Module Part
     Decoder decoder(
+        rst_n,
         mem_rdata_I,
         type,
         format,
@@ -127,6 +128,8 @@ module RISCV(clk,
         imm
     );
     Register register(
+        clk,
+        rst_n,
         RegWrite,
         read_reg1,
         read_reg2,
@@ -136,33 +139,48 @@ module RISCV(clk,
         read_data2
     );
     ALU alu(
+        rst_n,
         read_data1,
         reg_or_imm,
         ALU_ctrl,
         ALU_result,
         zero
     );
+    LE_CONV le0(
+        mem_rdata_D,
+        mem_rdata_D_BE
+    );
+    LE_CONV le1(
+        read_data2,
+        mem_wdata_D_LE
+    );
 
     // Combinational Part (PC controller)
     always@ (*) begin
         if (type == BEQ) begin
             if (zero) begin
-                next_mem_addr_I = mem_addr_I + shifted_imm;
+                next_mem_addr_I = mem_addr_I + (imm[29:0] >> 2);
             end
             else begin
-                next_mem_addr_I = mem_addr_I + 4;
+                next_mem_addr_I = mem_addr_I + 1;
             end
         end
         else if (type == BNE) begin
             if (!zero) begin
-                next_mem_addr_I = mem_addr_I + shifted_imm;
+                next_mem_addr_I = mem_addr_I + (imm[29:0] >> 2);
             end
             else begin
-                next_mem_addr_I = mem_addr_I + 4;
+                next_mem_addr_I = mem_addr_I + 1;
             end
         end
+        else if (Jal) begin
+            next_mem_addr_I = mem_addr_I + (imm[29:0] >> 2);
+        end
+        else if (Jalr) begin
+            next_mem_addr_I = read_data1 + (imm[29:0] >> 2);
+        end
         else begin
-            next_mem_addr_I = mem_addr_I + 4;
+            next_mem_addr_I = mem_addr_I + 1;
         end
     end
           
@@ -179,8 +197,9 @@ module RISCV(clk,
 
 endmodule
 
-// Decoder
-module Decoder( // for mem_I
+module Decoder( 
+                rst_n,
+                // for mem_I
                 mem_rdata_I,
                 // for result output
                 instruction_type,
@@ -190,7 +209,7 @@ module Decoder( // for mem_I
                 );
 
     // Input/Output Part
-    input         clk, rst_n;
+    input         rst_n;
     input  [31:0] mem_rdata_I;
 	output [22:0] instruction_type;
 	output [4:0]  instruction_format;
@@ -229,9 +248,9 @@ module Decoder( // for mem_I
     parameter AND = {22'b0, 1'b1};
 
     // Wire Part
-    wire [6:0] opcode = mem_rdata_I[6:0];
-    wire [2:0] func3  = mem_rdata_I[14:12];
-    wire [6:0] func7  = mem_rdata_I[31:25];
+    wire [6:0] opcode = mem_rdata_I[30:24];    // Little Endian
+    wire [2:0] func3  = mem_rdata_I[22:20];    // Little Endian
+    wire [6:0] func7  = mem_rdata_I[7:1];      // Little Endian
 
     // Reg Part
     reg [22:0] type;
@@ -252,17 +271,20 @@ module Decoder( // for mem_I
                 type = JAL;
                 format = J;
                 ctrl = 12'b100001111111;
-                immediate = { {43{mem_rdata_I[31]}}, mem_rdata_I[19:12], mem_rdata_I[20], mem_rdata_I[30:21], 1'b0 };
+                // Little Endian
+                imm = { {43{mem_rdata_I[7]}}, {mem_rdata_I[11:8], mem_rdata_I[23:20]}, mem_rdata_I[12], {mem_rdata_I[6:0], mem_rdata_I[15:13]}, 1'b0 };
             end
             7'b1100111: begin
                 type = JALR;
                 format = I;
                 ctrl = 12'b010001111111;
-                immediate = { {52{mem_rdata_I[31]}}, mem_rdata_I[31:20] };
+                // Little Endian
+                imm = { {52{mem_rdata_I[7]}}, {mem_rdata_I[7:0], mem_rdata_I[15:12]} };
             end
             7'b1100011: begin
                 format = B;
-                immediate = { {51{mem_rdata_I[31]}}, mem_rdata_I[31], mem_rdata_I[7], mem_rdata_I[30:25], mem_rdata_I[11:8], 1'b0 };
+                // Little Endian
+                imm = { {51{mem_rdata_I[7]}}, mem_rdata_I[7], mem_rdata_I[31], mem_rdata_I[6:1], mem_rdata_I[19:16], 1'b0 };
                 case (func3)
                     3'b000: begin
                         type = BEQ;
@@ -272,23 +294,30 @@ module Decoder( // for mem_I
                         type = BNE;
                         ctrl = 12'b001001001111;
                     end
+                    default: begin
+                        type = 23'b0;
+                        ctrl = 12'b0;
+                    end
                 endcase
             end
             7'b0000011: begin
                 type = LD;
                 format = I;
                 ctrl =  12'b000101111111;
-                immediate = { {52{mem_rdata_I[31]}}, mem_rdata_I[31:20] };
+                // Little Endian
+                imm = { {52{mem_rdata_I[7]}}, {mem_rdata_I[7:0], mem_rdata_I[15:12]} };
             end
             7'b0100011: begin
                 type = SD;
                 format = S;
                 ctrl = 12'b000011011111;
-                immediate = { {52{mem_rdata_I[31]}}, mem_rdata_I[31:25], mem_rdata_I[11:7] };
+                // Little Endian
+                imm = { {52{mem_rdata_I[7]}}, mem_rdata_I[7:1], {mem_rdata_I[19:16], mem_rdata_I[31]}};
             end
             7'b0010011: begin
                 format = I;
-                immediate = { {52{mem_rdata_I[31]}}, mem_rdata_I[31:20] };
+                // Little Endian
+                imm = { {52{mem_rdata_I[7]}}, {mem_rdata_I[7:0], mem_rdata_I[15:12]} };
                 case (func3)
                     3'b000: begin 
                         type = ADDI;
@@ -315,6 +344,11 @@ module Decoder( // for mem_I
                             7'b0100000: begin
                                 type = SRAI;
                                 ctrl = 12'b000000111101;
+                                imm = { {59{mem_rdata_I[0]}}, {mem_rdata_I[0], mem_rdata_I[15:12]} };
+                            end
+                            default: begin
+                                type = 23'b0;
+                                ctrl = 12'b0;
                             end
                         endcase
                     end
@@ -326,11 +360,15 @@ module Decoder( // for mem_I
                         type = ANDI;
                         ctrl = 12'b000000110111;
                     end
+                    default: begin
+                        type = 23'b0;
+                        ctrl = 12'b0;
+                    end
                 endcase
             end
             7'b0110011: begin
                 format = R;
-                immediate = 64'b0;
+                imm = 64'b0;
                 case (func3)
                     3'b000: begin
                         case (func7)
@@ -341,6 +379,10 @@ module Decoder( // for mem_I
                             7'b0100000: begin
                                 type = SUB;
                                 ctrl = 12'b000000101000;
+                            end
+                            default: begin
+                                type = 23'b0;
+                                ctrl = 12'b0;
                             end
                         endcase
                     end 
@@ -366,6 +408,10 @@ module Decoder( // for mem_I
                                 type = SRA;
                                 ctrl = 12'b000000101101;
                             end
+                            default: begin
+                                type = 23'b0;
+                                ctrl = 12'b0;
+                            end
                         endcase
                     end 
                     3'b110: begin
@@ -376,15 +422,34 @@ module Decoder( // for mem_I
                         type = AND;
                         ctrl = 12'b000000100111;
                     end
+                    default: begin
+                        type = 23'b0;
+                        ctrl = 12'b0;
+                    end
                 endcase
+            end
+            default: begin
+                format = 5'b0;
+                imm = 64'b0;
+                type = 23'b0;
+                ctrl = 12'b0;
             end
         endcase
     end 
+
+    // always@(negedge rst_n) begin
+    //     type <= 0;
+    //     format <= 0;
+    //     ctrl <= 0;
+    //     imm <= 0;
+    // end
+
 endmodule
 
 module Register(
+                clk,
                 rst_n,
-                Reg_Write,
+                RegWrite,
                 read_reg1,
                 read_reg2,
                 write_reg,
@@ -394,7 +459,9 @@ module Register(
                 );
     
     // Input/Output Part
-    input           Reg_Write;
+    input           clk;
+    input           rst_n;
+    input           RegWrite;
     input  [4:0]    read_reg1, read_reg2, write_reg;
     input  [63:0]   write_data;
     output [63:0]   read_data1, read_data2;
@@ -404,29 +471,35 @@ module Register(
 
     // Wire Part
     // Reg Part
-    reg [63:0] register [0:REGSIZE-1];
+    reg [63:0] register_r [0:REGSIZE-1];
+    reg [63:0] register_w [0:REGSIZE-1];
 
     // Assign Part
-    assign read_data1 = register[read_reg1];
-    assign read_data2 = register[read_reg2];
-
-    // Combinational Part
-    always @(*) begin
-        if (RegWrite && write_reg != 0) begin
-            register[write_reg] = write_data;
-        end
-        // else begin
-        // end
-    end
+    assign read_data1 = register_w[read_reg1];
+    assign read_data2 = register_w[read_reg2];
 
     integer i;
-    always @(negedge rst_n) begin
-        for (i=0; i<REGSIZE; i=i+1)
-            register[i] <= 64'b0;
+    // Combinational Part
+    always @(*) begin
+        for (i=0; i<REGSIZE; i=i+1) begin
+            register_w[i] = (RegWrite && (i == write_reg) && write_reg != 0) ? write_data : register_r[i];
+        end
+    end
+    // Sequential Part
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            for (i=0; i<REGSIZE; i=i+1)
+                register_r[i] <= 64'b0;
+        end
+        else begin
+            for (i=0; i<REGSIZE; i=i+1)
+                register_r[i] <= register_w[i];
+        end
     end
 endmodule
 
 module ALU(
+            rst_n,
             data1,
             data2,
             ctrl,  // ALU_ctrl
@@ -435,10 +508,11 @@ module ALU(
             );
 
     // Input/Output Part
+    input         rst_n;
     input  [63:0] data1, data2;
     input  [3:0]  ctrl;
     output [63:0] result;
-    output zero; // boolean
+    output        zero; // boolean
     
     // Parameter Part
     localparam ADD = 4'b0000;
@@ -469,6 +543,34 @@ module ALU(
             SRA: res = (data1 >>> data2);
             OR:  res = (data1 | data2);
             AND: res = (data1 & data2);
+            default: res = 64'b0;
         endcase
     end
+
+    // always@(negedge rst_n) begin
+    //     res <= 0;   
+    // end
+
+endmodule
+
+// little endian converter
+module LE_CONV(
+        in,
+        out
+        );
+        input  [63:0] in;
+        output [63:0] out;
+
+        wire   [63:0] tmp;
+
+        assign tmp[7:0]   = in[63:56];
+        assign tmp[15:8]  = in[55:48];
+        assign tmp[23:16] = in[47:40];
+        assign tmp[31:24] = in[39:32];
+        assign tmp[39:32] = in[31:24];
+        assign tmp[47:40] = in[23:16];
+        assign tmp[55:48] = in[15:8];
+        assign tmp[63:56] = in[7:0];
+        assign out = tmp;
+
 endmodule
